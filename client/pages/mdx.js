ee35cb5713c14d0e9ccd2b54ad67d5af58d0ec24
Ivan Kleshnin <ivan@paqmind.com>
2019-06-07 09:40:16 +0300

    Initial commit

---
+++ client/pages/mdx.js
@@ -0,0 +1,10 @@
+import React from "react"
+import * as Test from "../content-mdx/Test.mdx"
+
+console.log(Test)
+
+export default class MdxPage extends React.Component {
+  render() {
+    return <div>MdxPage</div>
+  }
+}
---
+++ client/pages/mdx2.js
@@ -0,0 +1,10 @@
+import React from "react"
+import * as Test from "../../content2-mdx/Test2.mdx"
+
+console.log(Test)
+
+export default class MdxPage extends React.Component {
+  render() {
+    return <div>MdxPage</div>
+  }
+}
